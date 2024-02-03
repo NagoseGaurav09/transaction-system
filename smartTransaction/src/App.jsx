542cf47726a666c7cc611c1b8dc5078e379ce27e
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-02-03 14:39:02 +0530

    bootstrap and react

---
+++ smartTransaction/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import  {Navbar}  from './components/Navbar';
+import {Hero} from './components/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+    
+
+
+  )
+}
+
+export default App
---
+++ smartTransaction/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+
+export const Hero = () => {
+    return (
+        <div >
+            <div>
+                <h1>SMART TRANSACTION SYSTEM</h1>
+                <p>We are introducing “ SMART TRANSACTION SYSTEM USING MERN STACK ”<br />A smart transaction system refers to a automated framework <br />designed to enhance and secure various type  of financial services. <br /> we all need to transaction of money with different secure way the <br />potential threats faced while performing Money transactions.</p>
+            </div>
+            <div >
+                
+            </div>
+        </div>
+    )
+}
