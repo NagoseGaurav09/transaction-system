dc8ecfa87872e1468726aaf6a2a1f8a0f4eb415f
NagoseGaurav09 <158729934+NagoseGaurav09@users.noreply.github.com>
2024-02-06 07:49:07 +0530

    done with pages and functions and modes

---
+++ smartTransaction/src/pages/Mode3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mode3 = () => {
+  return (
+    <div>Mode3</div>
+  )
+}
+
+export default Mode3
---
+++ smartTransaction/src/pages/Mode4.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mode4 = () => {
+  return (
+    <div>Mode4</div>
+  )
+}
+
+export default Mode4
